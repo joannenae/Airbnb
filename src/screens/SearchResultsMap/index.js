433d5170b7a0bb87b47efa2c00908eacf84757be
seunghyun <attosiss@dreamsecurity.com>
2023-08-02 14:42:26 +0900

    map and list sync

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {FlatList, useWindowDimensions, View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import feed from '../../../assets/data/feed';
@@ -8,11 +8,40 @@ import PostCarouselItem from '../../components/PostCarouselItem';
 const SearchResultsMap = () => {
   const [selectedPlaceId, setSelectedPlaceId] = useState(null);
 
+  const flatlist = useRef();
+  const map = useRef();
+
+  const viewConfig = useRef({itemVisiblePercentThreshold: 70});
+  const onViewChange = useRef(({viewableItems}) => {
+    if (viewableItems.length > 0) {
+      const selectedPlace = viewableItems[0].item;
+      setSelectedPlaceId(selectedPlace.id);
+    }
+  });
+
   const width = useWindowDimensions().width;
+
+  useEffect(() => {
+    if (!selectedPlaceId || !flatlist) {
+      return;
+    }
+    const index = feed.findIndex(place => place.id === selectedPlaceId);
+    flatlist.current.scrollToIndex({index});
+
+    const selectedPlace = feed[index];
+    const region = {
+      latitude: selectedPlace.coordinate.latitude,
+      longitude: selectedPlace.coordinate.longitude,
+      latitudeDelta: 0.8,
+      longitudeDelta: 0.8,
+    };
+    map.current.animateToRegion(region);
+  }, [selectedPlaceId]);
 
   return (
     <View style={{width: '100%', height: '100%'}}>
       <MapView
+        ref={map}
         provider={PROVIDER_GOOGLE}
         style={{width: '100%', height: '100%'}}
         initialRegion={{
@@ -34,12 +63,16 @@ const SearchResultsMap = () => {
 
       <View style={{position: 'absolute', bottom: 20}}>
         <FlatList
+          ref={flatlist}
           data={feed}
           renderItem={({item}) => <PostCarouselItem post={item} />}
           horizontal
           showsHorizontalScrollIndicator={false}
           snapToInterval={width - 60}
           snapToAlignment={'center'}
+          decelerationRate={'fast'}
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChange.current}
         />
       </View>
     </View>
